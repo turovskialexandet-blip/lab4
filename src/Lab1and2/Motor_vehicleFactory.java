e5d189561eba5d714cea8ce13e82e7bbbe7edd87
Alexander Turovski <turovskialexandet@gmail.com>
2026-02-26 14:40:37 +0100

    Update Motor_vehicleFactory.java!

--- src/Lab1and2/Motor_vehicleFactory.java
+++ src/Lab1and2/Motor_vehicleFactory.java
@@ -1,4 +1,13 @@
 package Lab1and2;
 
 public class Motor_vehicleFactory {
+    public Volvo240 createVolvo240(){
+        return new Volvo240();
+    }
+    public Saab95 createSaab95(){
+        return new Saab95();
+    }
+    public Scania createScania(){
+        return new Scania();
+    }
 }
